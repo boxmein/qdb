639028294a1548b1240ae6234c082d7885bf3c5a
boxmein <wsdf294@gmail.com>
2015-06-13 20:48:59 +0300

    add heroku tools to dev machine

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 echo "Installing Ruby and Postgres..."
-sudo apt-get -y install ruby ruby-dev postgresql libpq-dev
+sudo apt-get -y install ruby ruby-dev build-essential postgresql libpq-dev
 
 echo "Installing gems..."
 sudo gem install bundler
@@ -13,5 +13,9 @@ bundle install
 echo "Copying over environment variables..."
 
 cp /vagrant/environment_variables ~/.bash_profile
+
+echo "Installing Heroku toolchain..."
+
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sudo sh
 
 echo "Done!"
