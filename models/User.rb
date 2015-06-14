3c52e91242ad8db0eeb1319f196debeab5084929
boxmein <wsdf294@gmail.com>
2015-06-14 17:37:14 +0300

    added password verification routines inside models/User

--- models/User.rb
+++ models/User.rb
@@ -1,3 +1,12 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
+  def pw
+    @pw ||= BCrypt::Password.new self.password
+  end
+
+  def pw=(new_password)
+    @pw = BCrypt::Password.create new_password
+    self.password = @pw
+  end
 end
