f9f800dbe8bf1dcdc73af78b26100ab881d00a96
boxmein <wsdf294@gmail.com>
2015-06-15 23:40:49 +0300

    complex validation for users

--- models/User.rb
+++ models/User.rb
@@ -1,6 +1,13 @@
 require 'bcrypt'
 class User < ActiveRecord::Base
-  validates_presence_of :password, :on => :create
+  validates :name, presence: true
+  validates :password, presence: true
+
+  validates :name, format: {with: /\A[A-Za-z0-9\-_.]+\z/, message: 'The username contains invalid characters! Use /A-Za-z0-9-_./'}
+  validates :name, length: {minimum: 4, maximum: 16, message: 'The username is not the right size! Try 4-16 characters.'}
+  validates :name, uniqueness: {case_sensitive: false, message: 'The username has already been taken! Try another one!'}
+  validates :flags, numericality: {only_integer: true, message: 'The new flags were not a valid number.'}
+
   def pw
     @pw ||= BCrypt::Password.new self.password
   end
