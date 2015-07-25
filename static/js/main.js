18da598d1e6078b00f6709e2fa8d05dab7e2fddd
boxmein <wsdf294@gmail.com>
2015-07-26 00:25:15 +0300

    main.js now includes upvotees

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,32 @@
 window.addEventListener('load', $.material.init.bind($.material)); 
+// Upvoting functionality
+window.addEventListener('load', function() {
+  $('.upvotes').click(function(evt) {
+    var $that = $(evt.target);
+
+    if ($that.hasClass('.voted')) {
+      console.log('already voted!');
+      return;
+    }
+
+    var id = $that.parents('.quote').data('id');
+    console.log('upvoting id', id);
+
+    $.ajax({
+      url: '/upvote/' + id,
+      method: 'POST'
+    })
+    .error(console.error.bind(console))
+    .done(function(data) {
+      try { 
+        var obj = JSON.parse(data); 
+        $that.html('&and; ' + obj.votes_now);
+        $that.addClass('voted');
+      } catch (err) {
+        console.error(err);
+        $that.html('&and; err');
+        $that.addClass('error');
+      }
+    });
+  });
+});
