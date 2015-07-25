6b170aeba52138773edd9f944aa3f8b0542335ff
boxmein <wsdf294@gmail.com>
2015-07-25 23:23:40 +0300

    add an integer validation to upvotes, just in case

--- models/Quote.rb
+++ models/Quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
   validates :quote, presence: true
   validates :quote, length: {maximum: 1000}
+  validates :upvotes, numericality: { only_integer: true }
   has_many :votes
   has_many :voters, :through => :votes, source: :user, :class_name => 'User'
 end
