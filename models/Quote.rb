88d67303eace928bd115aeb02b2b84dbdcef5002
boxmein <wsdf294@gmail.com>
2015-11-10 00:50:17 +0200

    Added a few zeros to account for some quotes' .....generous-ness

--- models/Quote.rb
+++ models/Quote.rb
@@ -1,6 +1,6 @@
 class Quote < ActiveRecord::Base
   validates :quote, presence: true
-  validates :quote, length: {maximum: 1000}
+  validates :quote, length: {maximum: 30000}
   validates :upvotes, numericality: { only_integer: true }
   has_many :votes
   has_many :voters, :through => :votes, source: :user, :class_name => 'User'
